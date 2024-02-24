521cee4b06b8598a2d24807537ad70b5080f676c
David Roman <roman.david@gmail.com>
2024-02-23 20:18:13 -0500

    fix app

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <v-app-bar app>
       <v-tabs color="primary">
         <v-tab to="/"> Inicio </v-tab>
-        <v-tab v-if="userStore" to="/pedidos"> Pedidos </v-tab>
-        <v-tab v-if="userStore" to="/productos"> Productos </v-tab>
+        <v-tab to="/pedidos"> Pedidos </v-tab>
+        <v-tab to="/productos"> Productos </v-tab>
         <v-spacer></v-spacer>
-        <v-tab v-if="userStore" @click="logout"> Salir </v-tab>
+        <v-tab @click="logout"> Salir </v-tab>
       </v-tabs>
     </v-app-bar>
     <v-main>
